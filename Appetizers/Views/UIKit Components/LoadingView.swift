f73d8959fb7847f25f15f2c22c7ed538e794e774
enumcase <markdaribayev@gmail.com>
2021-04-11 02:36:36 +0600

    changed activity indicator progress view

--- Appetizers/Views/UIKit Components/LoadingView.swift
+++ Appetizers/Views/UIKit Components/LoadingView.swift
@@ -7,16 +7,16 @@
 
 import SwiftUI
 
-struct ActivityIndicator: UIViewRepresentable {
-    func makeUIView(context: Context) -> UIActivityIndicatorView {
-        let activityIndicatorView = UIActivityIndicatorView(style: .large)
-        activityIndicatorView.color = UIColor.brandPrimary
-        activityIndicatorView.startAnimating()
-        return activityIndicatorView
-    }
-    
-    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {}    
-}
+//struct ActivityIndicator: UIViewRepresentable {
+//    func makeUIView(context: Context) -> UIActivityIndicatorView {
+//        let activityIndicatorView = UIActivityIndicatorView(style: .large)
+//        activityIndicatorView.color = UIColor.brandPrimary
+//        activityIndicatorView.startAnimating()
+//        return activityIndicatorView
+//    }
+//    
+//    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {}    
+//}
 
 struct LoadingView: View {
     var body: some View {
@@ -24,7 +24,9 @@ struct LoadingView: View {
             Color(.systemBackground)
                 .edgesIgnoringSafeArea(.all)
             
-            ActivityIndicator()
+            ProgressView()
+                .progressViewStyle(CircularProgressViewStyle(tint: .brandPrimary))
+                .scaleEffect(1.5)
         }
     }
 }
