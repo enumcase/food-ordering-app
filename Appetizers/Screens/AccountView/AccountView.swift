15a37dc16f745f2655096ab89ad50217aced52bb
enumcase <markdaribayev@gmail.com>
2021-04-10 13:08:08 +0600

    completed UI of account view

--- Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Screens/AccountView/AccountView.swift
@@ -8,10 +8,36 @@
 import SwiftUI
 
 struct AccountView: View {
+    @StateObject var accountViewModel = AccountViewModel()
+    
     var body: some View {
         NavigationView {
-            Text("Account view")
+            Form {
+                Section(header: Text("Personal Info")) {
+                    TextField("First Name", text: $accountViewModel.firstName)
+                    TextField("Last Name", text: $accountViewModel.lastName)
+                    TextField("Email", text: $accountViewModel.email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                    DatePicker("Birthdate", selection: $accountViewModel.birthdate, displayedComponents: .date)
+                    Button {
+                        accountViewModel.didTapSaveChanges()
+                    } label: {
+                        Text("Save changes")
+                    }
+                }
+                
+                Section(header: Text("Requests")) {
+                    Toggle("Extra napkins", isOn: $accountViewModel.needExtraNapkins)
+                    Toggle("Extra food eating items", isOn: $accountViewModel.needExtraFoodEatingItems)
+                }
+                .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
+            }
             .navigationTitle("😎 Account")
+            .alert(item: $accountViewModel.alertItem) { alertItem in
+                Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
+            }
         }
     }
 }
