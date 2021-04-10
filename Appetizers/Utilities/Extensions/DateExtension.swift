dc4b3ae061a145520d1be9113491631726c11779
enumcase <markdaribayev@gmail.com>
2021-04-11 02:35:54 +0600

    added Date extension for default birthdate range

---
+++ Appetizers/Utilities/Extensions/DateExtension.swift
@@ -0,0 +1,20 @@
+//
+//  DateExtension.swift
+//  Appetizers
+//
+//  Created by Margulan Daribayev on 11.04.2021.
+//
+
+import Foundation
+
+extension Date {
+    
+    var eighteenYearsAgo: Date {
+        Calendar.current.date(byAdding: .year, value: -18, to: Date())!
+    }
+    
+    
+    var oneHundredTenYearsAgo: Date {
+        Calendar.current.date(byAdding: .year, value: -110, to: Date())!
+    }
+}
