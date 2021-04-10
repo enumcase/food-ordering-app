2e6f4db7865faa097e196035295195afbb905214
enumcase <markdaribayev@gmail.com>
2021-04-10 15:00:03 +0600

    updated mock data

--- Appetizers/Models/Appetizer.swift
+++ Appetizers/Models/Appetizer.swift
@@ -23,7 +23,7 @@ struct AppetizerResponse: Decodable {
 }
 
 struct MockData {
-    static let sampleAppetizer = Appetizer(id: 1,
+    static let sampleAppetizer1 = Appetizer(id: 1,
                                            name: "Asian Flank Steak",
                                            description: "This perfectly thin cut just melts in your mouth.",
                                            price: 8.99,
@@ -32,5 +32,23 @@ struct MockData {
                                            protein: 14,
                                            carbs: 0)
     
-    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+    static let sampleAppetizer2 = Appetizer(id: 2,
+                                           name: "Asian Flank Steak",
+                                           description: "This perfectly thin cut just melts in your mouth.",
+                                           price: 8.99,
+                                           imageURL: "food-placeholder",
+                                           calories: 300,
+                                           protein: 14,
+                                           carbs: 0)
+    
+    static let sampleAppetizer3 = Appetizer(id: 3,
+                                           name: "Asian Flank Steak",
+                                           description: "This perfectly thin cut just melts in your mouth.",
+                                           price: 8.99,
+                                           imageURL: "food-placeholder",
+                                           calories: 300,
+                                           protein: 14,
+                                           carbs: 0)
+    
+    static let appetizers = [sampleAppetizer1, sampleAppetizer2, sampleAppetizer3]
 }
