04121447e1c391d0baae9003c9eb369ba726a2db
enumcase <markdaribayev@gmail.com>
2021-04-11 11:33:13 +0600

    changed alert button's color

--- Appetizers/AppetizersApp.swift
+++ Appetizers/AppetizersApp.swift
@@ -8,7 +8,13 @@
 import SwiftUI
 
 @main
+
 struct AppetizersApp: App {
+    
+    init() {
+        UIView.appearance(whenContainedInInstancesOf: [UIAlertController.self]).tintColor = UIColor(Color.brandPrimary)
+    }
+    
     var order = Order()
     
     var body: some Scene {
@@ -16,4 +22,5 @@ struct AppetizersApp: App {
             AppetizerTabView().environmentObject(order)
         }
     }
+    
 }
